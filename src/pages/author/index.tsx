bbea673bf98ad4c37ea642c8d7a70d3a9a8b8dfe
BreathlessWay <731005087@qq.com>
2020-03-10 11:00:47 +0800

    perf: 添加动画

--- src/pages/author/index.tsx
+++ src/pages/author/index.tsx
@@ -1,4 +1,4 @@
-import React, { FC, useState } from 'react';
+import React, { FC, useRef, useState } from 'react';
 import { inject, observer } from 'mobx-react';
 
 import { useNavigation } from '@react-navigation/native';
@@ -10,11 +10,13 @@ import {
 	Dimensions,
 	TouchableOpacity,
 	Clipboard,
+	Animated,
 } from 'react-native';
 import ParallaxScrollView from 'react-native-parallax-scroll-view';
 import IonIcons from 'react-native-vector-icons/Ionicons';
 import AntDesign from 'react-native-vector-icons/AntDesign';
 import OctIcons from 'react-native-vector-icons/Octicons';
+import Toast from 'react-native-easy-toast';
 import MenuListItem from '@components/business/MenuListItem';
 
 import { Store } from '@/stores';
@@ -25,12 +27,21 @@ import { EScreenName, RootStackParamList } from '@routes/route.d';
 import Style, { stickyHeaderHeight } from './style';
 
 import aboutJson from '@config/about.json';
-import { handlePress } from 'react-native-paper/lib/typescript/src/components/RadioButton/utils';
+
+const ITEM_HEIGHT = 59,
+	ANIMATE_TIME = 300;
 
 const AuthorPage = () => {
-	const [showBlog, setShowBlog] = useState(false),
-		[showContact, setShowContact] = useState(false),
-		[showExchange, setShowExchange] = useState(false);
+	const [slidAnimBlog] = useState(new Animated.Value(0)),
+		[slidAnimToValueBlog, setSlidAnimToValueBlog] = useState(ITEM_HEIGHT * 2),
+		[slidAnimContact] = useState(new Animated.Value(0)),
+		[slidAnimToValueContact, setSlidAnimToValueContact] = useState(ITEM_HEIGHT),
+		[slidAnimExchange] = useState(new Animated.Value(0)),
+		[slidAnimToValueExchange, setSlidAnimToValueExchange] = useState(
+			ITEM_HEIGHT,
+		);
+
+	const ref = useRef<Toast>();
 
 	const navigation = useNavigation<StackNavigationProp<RootStackParamList>>();
 
@@ -54,104 +65,152 @@ const AuthorPage = () => {
 			});
 		} else {
 			Clipboard.setString(value);
+			ref.current?.show('复制成功');
 		}
 	};
 
 	return (
-		<ParallaxScrollView
-			backgroundColor="#678"
-			contentBackgroundColor="#f3f3f4"
-			parallaxHeaderHeight={350}
-			stickyHeaderHeight={stickyHeaderHeight}
-			backgroundScrollSpeed={10}
-			renderStickyHeader={() => (
-				<View key="sticky-header" style={Style.stickySection}>
-					<Text style={Style.stickySectionText}>{aboutJson.author.name}</Text>
-				</View>
-			)}
-			renderBackground={() => (
+		<View style={{ flex: 1 }}>
+			<ParallaxScrollView
+				backgroundColor="#678"
+				contentBackgroundColor="#f3f3f4"
+				parallaxHeaderHeight={350}
+				stickyHeaderHeight={stickyHeaderHeight}
+				backgroundScrollSpeed={10}
+				renderStickyHeader={() => (
+					<View key="sticky-header" style={Style.stickySection}>
+						<Text style={Style.stickySectionText}>{aboutJson.author.name}</Text>
+					</View>
+				)}
+				renderBackground={() => (
+					<View>
+						<Image
+							source={{
+								uri: aboutJson.author.backgroundImg,
+								width: Dimensions.get('window').width,
+								height: 350,
+							}}
+						/>
+					</View>
+				)}
+				renderForeground={() => (
+					<View style={Style.parallaxHeader}>
+						<Image
+							style={Style.avatar}
+							source={{
+								uri: aboutJson.author.avatar,
+							}}
+						/>
+						<Text style={Style.sectionSpeakerText}>
+							{aboutJson.author.name}
+						</Text>
+						<Text style={Style.sectionTitleText}>
+							{aboutJson.author.description}
+						</Text>
+					</View>
+				)}
+				renderFixedHeader={() => (
+					<View style={Style.fixedSection}>
+						<TouchableOpacity onPress={handleBack}>
+							<IonIcons name={'ios-arrow-back'} size={20} color="#fff" />
+						</TouchableOpacity>
+						<TouchableOpacity onPress={handleShare}>
+							<IonIcons name={'md-share'} size={20} color="#fff" />
+						</TouchableOpacity>
+					</View>
+				)}>
 				<View>
-					<Image
-						source={{
-							uri: aboutJson.author.backgroundImg,
-							width: Dimensions.get('window').width,
-							height: 350,
-						}}
+					<MenuListItem
+						name={aboutJson.aboutMe.Blog.name}
+						Icons={IonIcons}
+						icon={aboutJson.aboutMe.Blog.icon}
+						onPress={() =>
+							Animated.timing(
+								// 随时间变化而执行动画
+								slidAnimBlog, // 动画中的变量值
+								{
+									toValue: slidAnimToValueBlog, // 透明度最终变为1，即完全不透明
+									duration: ANIMATE_TIME, // 让动画持续一段时间
+								},
+							).start(() => {
+								slidAnimToValueBlog
+									? setSlidAnimToValueBlog(0)
+									: setSlidAnimToValueBlog(ITEM_HEIGHT * 2);
+							})
+						}
+						column={true}
 					/>
-				</View>
-			)}
-			renderForeground={() => (
-				<View style={Style.parallaxHeader}>
-					<Image
-						style={Style.avatar}
-						source={{
-							uri: aboutJson.author.avatar,
-						}}
+					<Animated.View style={{ height: slidAnimBlog }}>
+						{aboutJson.aboutMe.Blog.items.map(item => (
+							<MenuListItem
+								name={item.title}
+								wrapStyle={{ paddingLeft: 32 }}
+								onPress={() => handlePressItem(item)}
+							/>
+						))}
+					</Animated.View>
+					<MenuListItem
+						name={aboutJson.aboutMe.Contact.name}
+						Icons={AntDesign}
+						icon={aboutJson.aboutMe.Contact.icon}
+						onPress={() =>
+							Animated.timing(
+								// 随时间变化而执行动画
+								slidAnimContact, // 动画中的变量值
+								{
+									toValue: slidAnimToValueContact, // 透明度最终变为1，即完全不透明
+									duration: ANIMATE_TIME, // 让动画持续一段时间
+								},
+							).start(() => {
+								slidAnimToValueContact
+									? setSlidAnimToValueContact(0)
+									: setSlidAnimToValueContact(ITEM_HEIGHT);
+							})
+						}
+						column={true}
 					/>
-					<Text style={Style.sectionSpeakerText}>{aboutJson.author.name}</Text>
-					<Text style={Style.sectionTitleText}>
-						{aboutJson.author.description}
-					</Text>
+					<Animated.View style={{ height: slidAnimContact }}>
+						{aboutJson.aboutMe.Contact.items.map(item => (
+							<MenuListItem
+								name={item.title + ': ' + item.value}
+								wrapStyle={{ paddingLeft: 32 }}
+								onPress={() => handlePressItem(item)}
+							/>
+						))}
+					</Animated.View>
+					<MenuListItem
+						name={aboutJson.aboutMe.QQ.name}
+						Icons={OctIcons}
+						icon={aboutJson.aboutMe.QQ.icon}
+						onPress={() =>
+							Animated.timing(
+								// 随时间变化而执行动画
+								slidAnimExchange, // 动画中的变量值
+								{
+									toValue: slidAnimToValueExchange, // 透明度最终变为1，即完全不透明
+									duration: ANIMATE_TIME, // 让动画持续一段时间
+								},
+							).start(() => {
+								slidAnimToValueExchange
+									? setSlidAnimToValueExchange(0)
+									: setSlidAnimToValueExchange(ITEM_HEIGHT);
+							})
+						}
+						column={true}
+					/>
+					<Animated.View style={{ height: slidAnimExchange }}>
+						{aboutJson.aboutMe.QQ.items.map(item => (
+							<MenuListItem
+								name={item.title + ': ' + item.value}
+								wrapStyle={{ paddingLeft: 32 }}
+								onPress={() => handlePressItem(item)}
+							/>
+						))}
+					</Animated.View>
 				</View>
-			)}
-			renderFixedHeader={() => (
-				<View style={Style.fixedSection}>
-					<TouchableOpacity onPress={handleBack}>
-						<IonIcons name={'ios-arrow-back'} size={20} color="#fff" />
-					</TouchableOpacity>
-					<TouchableOpacity onPress={handleShare}>
-						<IonIcons name={'md-share'} size={20} color="#fff" />
-					</TouchableOpacity>
-				</View>
-			)}>
-			<View>
-				<MenuListItem
-					name={aboutJson.aboutMe.Blog.name}
-					Icons={IonIcons}
-					icon={aboutJson.aboutMe.Blog.icon}
-					onPress={() => setShowBlog(!showBlog)}
-					column={true}
-				/>
-				{showBlog &&
-					aboutJson.aboutMe.Blog.items.map(item => (
-						<MenuListItem
-							name={item.title}
-							wrapStyle={{ paddingLeft: 32 }}
-							onPress={() => handlePressItem(item)}
-						/>
-					))}
-				<MenuListItem
-					name={aboutJson.aboutMe.Contact.name}
-					Icons={AntDesign}
-					icon={aboutJson.aboutMe.Contact.icon}
-					onPress={() => setShowContact(!showContact)}
-					column={true}
-				/>
-				{showContact &&
-					aboutJson.aboutMe.Contact.items.map(item => (
-						<MenuListItem
-							name={item.title + ': ' + item.value}
-							wrapStyle={{ paddingLeft: 32 }}
-							onPress={() => handlePressItem(item)}
-						/>
-					))}
-				<MenuListItem
-					name={aboutJson.aboutMe.QQ.name}
-					Icons={OctIcons}
-					icon={aboutJson.aboutMe.QQ.icon}
-					onPress={() => setShowExchange(!showExchange)}
-					column={true}
-				/>
-				{showExchange &&
-					aboutJson.aboutMe.QQ.items.map(item => (
-						<MenuListItem
-							name={item.title + ': ' + item.value}
-							wrapStyle={{ paddingLeft: 32 }}
-							onPress={() => handlePressItem(item)}
-						/>
-					))}
-			</View>
-		</ParallaxScrollView>
+			</ParallaxScrollView>
+			<Toast ref={ref as any} position="center" />
+		</View>
 	);
 };
 
